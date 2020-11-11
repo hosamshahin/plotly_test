dde8e06bde8a0c9310b960da66b54bb5504a5726
hshahin <hshahin@vt.edu>
2020-11-10 19:29:36 -0500

    Add dropDown

--- index.js
+++ index.js
@@ -1,8 +1,33 @@
-var yData = [3.2146905937041748, 16.337442378832883, 3.2539601774461735, 4.755584638603736, 15.956184503328522, 1.0236859132489684, 6.423572479978357, 19.933330118912536, 6.2264725816196265, 10.274937628559645, 8.047246143373282, 9.175666737991758, 18.645787209731942, 18.567844540339404, 17.528679104158215, 0.5182648760324993, 3.8180323515972248, 15.527970648790216, 19.753525357400772, 2.781231640974644, 12.854973038577594, 12.827236979598968, 5.496562187336775, 9.653908256257871, 13.86613500036622, 18.408012701634846, 18.7709252038373, 12.40018253575155, 17.028683936654602, 14.459569438850227, 7.365172492737981];
-var yData1 = [3.2146905937041748, 15.337442378832883, 13.2539601774461735, 14.755584638603736, 5.956184503328522, 11.0236859132489684, 16.423572479978357, 10.933330118912536, 6.2264725816196265, 10.274937628559645, 8.047246143373282, 9.175666737991758, 18.645787209731942, 18.567844540339404, 17.528679104158215, 0.5182648760324993, 3.8180323515972248, 15.527970648790216, 19.753525357400772, 2.781231640974644, 12.854973038577594, 12.827236979598968, 5.496562187336775, 9.653908256257871, 13.86613500036622, 18.408012701634846, 18.7709252038373, 12.40018253575155, 17.028683936654602, 14.459569438850227, 7.365172492737981];
+var listOfStudents = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+var innerContainer = document.querySelector('[data-num="0"');
+var studentSelector = innerContainer.querySelector('.student_data');
 
-var data = [
-  {
+function assignOptions(textArray, selector) {
+  for (var i = 0; i < textArray.length; i++) {
+    var currentOption = document.createElement('option');
+    currentOption.text = textArray[i];
+    selector.appendChild(currentOption);
+  }
+}
+
+assignOptions(listOfStudents, studentSelector);
+
+function updatePlot() {
+  setBubblePlot(studentSelector.value);
+}
+
+studentSelector.addEventListener('change', updatePlot, false);
+
+
+// Default Country Data
+setBubblePlot();
+
+function setBubblePlot(chosenStudent) {
+  var selectedpoints = (chosenStudent) ? [chosenStudent] : [];
+  var yData = [3.2146905937041748, 16.337442378832883, 3.2539601774461735, 4.755584638603736, 15.956184503328522, 1.0236859132489684, 6.423572479978357, 19.933330118912536, 6.2264725816196265, 10.274937628559645, 8.047246143373282, 9.175666737991758, 18.645787209731942, 18.567844540339404, 17.528679104158215, 0.5182648760324993, 3.8180323515972248, 15.527970648790216, 19.753525357400772, 2.781231640974644, 12.854973038577594, 12.827236979598968, 5.496562187336775, 9.653908256257871, 13.86613500036622, 18.408012701634846, 18.7709252038373, 12.40018253575155, 17.028683936654602, 14.459569438850227, 7.365172492737981];
+  var yData1 = [3.2146905937041748, 15.337442378832883, 13.2539601774461735, 14.755584638603736, 5.956184503328522, 11.0236859132489684, 16.423572479978357, 10.933330118912536, 6.2264725816196265, 10.274937628559645, 8.047246143373282, 9.175666737991758, 18.645787209731942, 18.567844540339404, 17.528679104158215, 0.5182648760324993, 3.8180323515972248, 15.527970648790216, 19.753525357400772, 2.781231640974644, 12.854973038577594, 12.827236979598968, 5.496562187336775, 9.653908256257871, 13.86613500036622, 18.408012701634846, 18.7709252038373, 12.40018253575155, 17.028683936654602, 14.459569438850227, 7.365172492737981];
+
+  var data = [{
     name: 'Week 1',
     type: 'box',
     y: yData,
@@ -22,7 +47,7 @@ var data = [
     },
     // boxmean: true,
     // notched: true,
-    // selectedpoints: [1],
+    selectedpoints: selectedpoints,
     selected: {
       marker: {
         size: 6,
@@ -59,7 +84,7 @@ var data = [
       opacity: 1
     },
     // notched: true,
-    // selectedpoints: [1],
+    selectedpoints: selectedpoints,
     selected: {
       marker: {
         size: 6,
@@ -79,28 +104,26 @@ var data = [
       font: { size: 15 }
     },
     visible: false
-  }
-];
+  }];
 
-var updatemenus = [
-  {
-    buttons: [
+  var updatemenus = [{
+    buttons: [{
+      args: [{ 'visible': [true, false] },
       {
-        args: [{ 'visible': [true, false] },
-        {
-          'title': 'Total time students spend on OpenDSA materials per week.'
-        }],
-        label: 'Weeks',
-        method: 'update'
-      }, {
-        args: [{ 'visible': [false, true] },
-        {
-          'title': 'Total time students spend on OpenDSA materials per chapter.'
-        }],
-        label: 'Chapters',
-        method: 'update'
+        'title': 'Total time students spend on OpenDSA materials per week.'
       }
-    ],
+      ],
+      label: 'Weeks',
+      method: 'update'
+    }, {
+      args: [{ 'visible': [false, true] },
+      {
+        'title': 'Total time students spend on OpenDSA materials per chapter.'
+      }
+      ],
+      label: 'Chapters',
+      method: 'update'
+    }],
     direction: 'left',
     pad: { 'r': 10, 't': 10 },
     showactive: true,
@@ -109,31 +132,31 @@ var updatemenus = [
     xanchor: 'left',
     y: 1.2,
     yanchor: 'top'
-  }
-]
+  }]
 
-var layout = {
-  'title': 'Total time students spend on OpenDSA materials per week.',
-  updatemenus: updatemenus,
-  yaxis: {
-    autorange: true,
-    showgrid: true,
-    zeroline: true,
-    dtick: 5,
-    gridcolor: 'rgb(255, 255, 255)',
-    gridwidth: 1,
-    zerolinecolor: 'rgb(255, 255, 255)',
-    zerolinewidth: 2
-  },
-  margin: {
-    l: 40,
-    r: 30,
-    b: 80,
-    t: 100
-  },
-  paper_bgcolor: 'rgb(243, 243, 243)',
-  plot_bgcolor: 'rgb(243, 243, 243)',
-  showlegend: false
+  var layout = {
+    'title': 'Total time students spend on OpenDSA materials per week.',
+    updatemenus: updatemenus,
+    yaxis: {
+      autorange: true,
+      showgrid: true,
+      zeroline: true,
+      dtick: 5,
+      gridcolor: 'rgb(255, 255, 255)',
+      gridwidth: 1,
+      zerolinecolor: 'rgb(255, 255, 255)',
+      zerolinewidth: 2
+    },
+    margin: {
+      l: 40,
+      r: 30,
+      b: 80,
+      t: 100
+    },
+    paper_bgcolor: 'rgb(243, 243, 243)',
+    plot_bgcolor: 'rgb(243, 243, 243)',
+    showlegend: false
+  };
+
+  Plotly.newPlot('myDiv', data, layout);
 };
-
-var pt = Plotly.newPlot('myDiv', data, layout);
